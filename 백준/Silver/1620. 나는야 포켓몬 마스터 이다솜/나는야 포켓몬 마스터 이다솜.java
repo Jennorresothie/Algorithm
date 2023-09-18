e4d9f4e5b306ccf2d4a9e43619451e1acceb2c2e
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2023-09-18 22:21:45 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 1180 ms, Memory: 45108 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -12,20 +12,20 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
 
         HashMap<String, Integer> nameMap = new HashMap<>();
-        HashMap<Integer, String> numMap = new HashMap<>();
+        String[] poket = new String[100001];
 
         String name;
         for(int i=1; i<=n; i++) {
             name = br.readLine();
             nameMap.put(name, i);
-            numMap.put(i, name);
+            poket[i] = name;
         }
 
         for(int i=0; i<m; i++){
             String cmd = br.readLine();
             if('0'<=cmd.charAt(0)&&cmd.charAt(0)<='9'){
                 int num = Integer.parseInt(cmd);
-                System.out.println(numMap.get(num));
+                System.out.println(poket[num]);
             }
             else{
                 System.out.println(nameMap.get(cmd));
