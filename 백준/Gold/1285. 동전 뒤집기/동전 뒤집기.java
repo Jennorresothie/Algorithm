c14b24c9e020fd54adde254f4807aa8ffa04f108
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-02-28 10:57:57 +0900

    [Gold I] Title: 동전 뒤집기, Time: 524 ms, Memory: 15820 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
@@ -1,55 +1,58 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n, array[], ret =  Integer.MAX_VALUE;
+    static final int INF = Integer.MAX_VALUE;
+    static int n, arr[], ret=INF;
+
+    static void bt(int depth){
+        // 모든 행을 다 변경했다면
+        if(depth>=n){
+            // 열 단위로 확인하면서 T가 더 적은 것으로 T개수 계산
+            int sum=0;
+            for(int i=1; i<(1<<n); i*=2) {
+                int cnt = 0;
+                for(int j=0; j<n; j++){
+                    if((arr[j]&i)>0)
+                        cnt++;
+                }
+                sum += Math.min(cnt, n-cnt);
+            }
+
+            ret=Math.min(ret, sum);
+
+            return;
+        }
+
+        bt(depth+1);
+        arr[depth] = ~arr[depth];
+        bt(depth+1);
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         // 입력
-        n = Integer.parseInt(br.readLine());
-        array = new int[n];
+        n=stoi(br.readLine());
+        arr = new int[n];
 
         for(int i=0; i<n; i++){
-            String str = br.readLine();
-            //이진수 -> 십진수
-            int bit = 1;
-            for(char c : str.toCharArray()){
-                if(c=='T') {
-                    array[i]|=bit;
-                }
+            //  H : 0, T : 1 로 이진수로 보고 십진수로 변환
+            int bit=1;
+            String coin = br.readLine();
+            for(char c : coin.toCharArray()){
+                if(c=='T')
+                    arr[i] |= bit;
                 bit*=2;
             }
         }
 
-        // 행만을 바꾸는 모든 경우의 수를 구함
-        for(int i=0; i<(1<<n); i++){
-            for(int j=0; j<n; j++) {
-                if((i&(1<<j))!=0){
-                    array[j]=~array[j];
-                }
-            }
-
-            // 열의 T의 개수를 세서 가장 작을 경우의 합을 구해서 비교
-            int sum=0;
-            for(int j=1; j<=(1<<(n-1)); j*=2) {
-                int cnt=0;
-                for(int k=0; k<n; k++){
-                    if((array[k]&j)>0) cnt++; // 열로 보면서 T 개수 카운트
-                }
-                sum+=Math.min(cnt, n-cnt);
-            }
-            ret=Math.min(sum, ret);
-
-            for(int j=0; j<n; j++) {
-                if((i&(1<<j))!=0){
-                    array[j]=~array[j];
-                }
-            }
-        }
-
+        bt(0);
         System.out.println(ret);
 
+    }
+
+    // 정수로 변환하는 함수
+    static int stoi(String str){
+        return Integer.parseInt(str);
     }
 }
