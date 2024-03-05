a4fa90beffb3ee4eade9ced4b6a61ebfbfd5de14
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-03-05 16:38:52 +0900

    [Gold III] Title: 현욱은 괄호왕이야!!, Time: 236 ms, Memory: 24288 KB -BaekjoonHub

---
+++ 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int n, ret;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n=Integer.parseInt(br.readLine());
+        char[] chars = br.readLine().toCharArray();
+        int size = chars.length;
+        boolean[] check = new boolean[size];
+        Stack<Integer> s = new Stack<>();
+
+        for(int i=0; i<size; i++){
+            if(chars[i]=='(') s.push(i);
+            else if(chars[i]==')'&&!s.empty()) {
+                check[s.pop()]=true;
+                check[i]=true;
+            }
+        }
+
+        int cnt=0;
+        for(boolean b: check){
+            if(b){
+                cnt++;
+                ret = Math.max(cnt,ret);
+            }
+                
+            else cnt=0;
+            
+        }
+
+        System.out.println(ret);
+    }
+}
