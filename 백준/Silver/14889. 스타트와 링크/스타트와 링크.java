b7fd7400fa0666d104cf2cc34e05515f7cab58ec
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-03-26 12:05:15 +0900

    [Silver I] Title: 스타트와 링크, Time: 136 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,65 +1,32 @@
 import java.io.*;
 
 public class Main {
-
-    static final int INF = Integer.MAX_VALUE;
-    static int n, stat[][], team[][] = new int[2][11], ret=INF;
-
-
-    static int compare(){
-        int team1, team2;
-        team1 = team2 = 0;
-        for(int i=0; i<n/2; i++) {
-            for(int j=0; j<n/2; j++) {
-                if(i!=j) {
-                    team1 += stat[team[0][i]][team[0][j]];
-                    team2 += stat[team[1][i]][team[1][j]];
-                }
-            }
-        }
-        return Math.abs(team1-team2);
-    }
-
+    static int n, total, row[] = new int[20], col[] = new int[20], ret=987654321;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         n = Integer.parseInt(br.readLine());
-        stat = new int[n][n];
         for(int i=0; i<n; i++) {
-            String[] strs = br.readLine().split(" ");
-            for(int j=0; j<n; j++)
-                stat[i][j] = Integer.parseInt(strs[j]);
-        }
-
-        long child=n, parent=n/2;
-        for(int i=1; i<n/2; i++){
-            child*=(n-i);
-            parent*=i;
-        }
-        int cnt=(int)(child/parent)/2;
-
-        for(int i=0; i<(1<<n); i++) {
-            if(cnt==0) break;
-            if(bitCheck(i)==n/2) {
-                int ind, arr, arr2;
-                ind = arr = arr2 = 0;
-                for(int j=1; j<(1<<n); j*=2){
-                    if((i&j)>0) team[0][arr++] = ind++;
-                    else team[1][arr2++] = ind++;
-                }
-                ret = Math.min(ret,compare());
-                cnt--;
+            String[] str = br.readLine().split(" ");
+            for(int j=0; j<n; j++) {
+                int v = Integer.parseInt(str[j]);
+                total += v;
+                row[i] += v;
+                col[j] += v;
             }
         }
+        match(1,1, total-row[0]-col[0]);
         bw.write(String.valueOf(ret));
         bw.close();
     }
-    static int bitCheck(int num) {
-        int cnt = 0;
-        for(int i=1; i<(1<<n); i*=2) {
-            if((num&i)>0) cnt++;
+    static void match(int ind, int depth, int cur) {
+        if(depth==n/2) {
+            ret = Math.min(ret, Math.abs(cur));
+            return;
         }
-        return cnt;
+        if(ind==n) return;
+        match(ind+1, depth+1, cur-row[ind]-col[ind]);
+        match(ind+1, depth, cur);
     }
 }
