76236a6f86e20958a5d8767a5484ca627f4f98b9
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-04-24 16:48:30 +0900

    [Gold II] Title: 피자판매, Time: 188 ms, Memory: 20596 KB -BaekjoonHub

--- 백준/Gold/2632. 피자판매/피자판매.java
+++ 백준/Gold/2632. 피자판매/피자판매.java
@@ -1,44 +1,41 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int p, n, m, a[], b[], ret;
-    static Map<Integer, Integer> aCnt, bCnt;
+    static int p, n, m, ret, narr[], marr[], nCnt[], mCnt[];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         p = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
-        a = new int[m*2+1];
-        b = new int[n*2+1];
-        for(int i=1; i<=m; i++) {
-            a[i] += Integer.parseInt(br.readLine());
-            a[i+m] = a[i];
-        }
-        for(int i=1; i<=n; i++) {
-            b[i] = Integer.parseInt(br.readLine());
-            b[i+n] = b[i];
-        }
-        for(int i=1; i<=m*2; i++) a[i] += a[i-1];
-        for(int i=1; i<=n*2; i++) b[i] += b[i-1];
-        aCnt = new HashMap<>();
-        bCnt = new HashMap<>();
-        make(m, aCnt, a);
-        make(n, bCnt, b);
-        ret = aCnt.getOrDefault(p, 0) + bCnt.getOrDefault(p, 0);
-        for(int i=1; i<p; i++) ret += (aCnt.getOrDefault(i,0)*bCnt.getOrDefault(p-i,0));
+        m = Integer.parseInt(st.nextToken());
+        narr = new int[n*2+1];
+        marr = new int[m*2+1];
+        nCnt = new int[p+1];
+        mCnt = new int[p+1];
+        for(int i=1; i<=n; i++) narr[i+n] = narr[i] = Integer.parseInt(br.readLine());
+        for(int i=1; i<=m; i++) marr[i+m] = marr[i] = Integer.parseInt(br.readLine());
+        makeSum(n, narr, nCnt);
+        makeSum(m, marr, mCnt);
+        ret = nCnt[p] + mCnt[p];
+        for(int i=1; i<p; i++) ret += (nCnt[i]*mCnt[p-i]);
+
         bw.write(String.valueOf(ret));
         bw.close();
     }
-    static void make(int len, Map<Integer, Integer> map, int[] arr){
-        for(int 묶음개수 = 1; 묶음개수 <= len; 묶음개수++) {
-            for(int startInd = 묶음개수; startInd<=len+묶음개수-1; startInd++) {
-                int sum = arr[startInd] - arr[startInd-묶음개수];
-                map.put(sum, map.getOrDefault(sum, 0)+1);
-                if(묶음개수==len) break;
+
+    static void makeSum(int size, int[] arr, int[] cnt){
+        for(int i=1; i<=size*2; i++) arr[i]+=arr[i-1];
+
+        for(int i=1; i<=size; i++) {
+            for(int j=i; j<=size+i-1; j++) {
+                int temp = arr[j] - arr[j-i];
+                if(temp>p) continue;
+                cnt[temp]++;
+                if(size==i)break;
             }
         }
+
     }
+
 }
