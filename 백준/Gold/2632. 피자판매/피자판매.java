c98dca925bb693bba19d8ddc442675693dc7e2a9
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-04-25 16:10:43 +0900

    [Gold II] Title: 피자판매, Time: 180 ms, Memory: 20588 KB -BaekjoonHub

--- 백준/Gold/2632. 피자판매/피자판매.java
+++ 백준/Gold/2632. 피자판매/피자판매.java
@@ -6,35 +6,39 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         p = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        narr = new int[n*2+1];
-        marr = new int[m*2+1];
         nCnt = new int[p+1];
         mCnt = new int[p+1];
+        String[] str = br.readLine().split(" ");
+        n = Integer.parseInt(str[0]);
+        m = Integer.parseInt(str[1]);
+        narr = new int[n*2+1];
+        marr = new int[m*2+1];
+
         for(int i=1; i<=n; i++) narr[i+n] = narr[i] = Integer.parseInt(br.readLine());
         for(int i=1; i<=m; i++) marr[i+m] = marr[i] = Integer.parseInt(br.readLine());
-        makeSum(n, narr, nCnt);
-        makeSum(m, marr, mCnt);
+
+        makeArr(n, narr, nCnt);
+        makeArr(m, marr, mCnt);
+
         ret = nCnt[p] + mCnt[p];
-        for(int i=1; i<p; i++) ret += (nCnt[i]*mCnt[p-i]);
+        for(int i=1; i<p; i++) ret+=(nCnt[i]*mCnt[p-i]);
 
         bw.write(String.valueOf(ret));
         bw.close();
     }
 
-    static void makeSum(int size, int[] arr, int[] cnt){
-        for(int i=1; i<=size*2; i++) arr[i]+=arr[i-1];
+    static void makeArr(int size, int[] arr, int[] cnt) {
+        // 누적합
+        for(int i=2; i<=size*2; i++) arr[i] += arr[i-1];
 
+        // 모든 가능성 구하기
         for(int i=1; i<=size; i++) {
             for(int j=i; j<=size+i-1; j++) {
                 int temp = arr[j] - arr[j-i];
-                if(temp <=p) cnt[temp]++;
-                if(size==i)break;
+                if(temp<=p) cnt[temp]++;
+                if(size==i) break;
             }
         }
-
     }
 
 }
