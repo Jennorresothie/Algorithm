4b6c163a804f9f1fdd2301bf61348369e225ceba
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-05-01 02:53:25 +0900

    [Silver II] Title: 연속합, Time: 272 ms, Memory: 23444 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -2,15 +2,15 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, ret = Integer.MIN_VALUE, sum;
+    static int ret = Integer.MIN_VALUE, sum;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        while(st.hasMoreTokens()) {
+        for(int i=0; i<n; i++) {
             sum += Integer.parseInt(st.nextToken());
-            if(sum>ret) ret=sum;
+            if(ret<sum) ret = sum;
             if(sum<0) sum=0;
         }
         bw.write(String.valueOf(ret));
