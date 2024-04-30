04fd163110d87e07de5bac99a13594ff27f08640
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-04-30 16:48:03 +0900

    [Gold II] Title: 피자판매, Time: 180 ms, Memory: 20588 KB -BaekjoonHub

--- 백준/Gold/2632. 피자판매/피자판매.java
+++ 백준/Gold/2632. 피자판매/피자판매.java
@@ -1,44 +1,36 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int p, n, m, ret, narr[], marr[], nCnt[], mCnt[];
+    static int p,n,m, ret, arr[], brr[], aCnt[], bCnt[];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         p = Integer.parseInt(br.readLine());
-        nCnt = new int[p+1];
-        mCnt = new int[p+1];
-        String[] str = br.readLine().split(" ");
-        n = Integer.parseInt(str[0]);
-        m = Integer.parseInt(str[1]);
-        narr = new int[n*2+1];
-        marr = new int[m*2+1];
-
-        for(int i=1; i<=n; i++) narr[i+n] = narr[i] = Integer.parseInt(br.readLine());
-        for(int i=1; i<=m; i++) marr[i+m] = marr[i] = Integer.parseInt(br.readLine());
-
-        makeArr(n, narr, nCnt);
-        makeArr(m, marr, mCnt);
-
-        ret = nCnt[p] + mCnt[p];
-        for(int i=1; i<p; i++) ret+=(nCnt[i]*mCnt[p-i]);
-
+        aCnt = new int[p+1];
+        bCnt = new int[p+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr=new int[n*2+1];
+        brr=new int[m*2+1];
+        for(int i=1; i<=n; i++) arr[i+n]=arr[i]=Integer.parseInt(br.readLine());
+        for(int i=1; i<=m; i++) brr[i+m]=brr[i]=Integer.parseInt(br.readLine());
+        returnCnt(n, arr, aCnt);
+        returnCnt(m, brr, bCnt);
+        ret = aCnt[p]+bCnt[p];
+        for(int i=1; i<p; i++) ret+=(aCnt[i]*bCnt[p-i]);
         bw.write(String.valueOf(ret));
         bw.close();
     }
+    static void returnCnt(int size, int[] array, int[] cnt){
+        for(int i=2; i<=size*2; i++) array[i]+=array[i-1];
 
-    static void makeArr(int size, int[] arr, int[] cnt) {
-        // 누적합
-        for(int i=2; i<=size*2; i++) arr[i] += arr[i-1];
-
-        // 모든 가능성 구하기
         for(int i=1; i<=size; i++) {
             for(int j=i; j<=size+i-1; j++) {
-                int temp = arr[j] - arr[j-i];
+                int temp = array[j] - array[j-i];
                 if(temp<=p) cnt[temp]++;
-                if(size==i) break;
+                if(i==size) break;
             }
         }
     }
-
 }
