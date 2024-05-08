065243a9898d20f066e6954b7530b37fab21992b
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-05-09 03:16:36 +0900

    [Gold II] Title: 원판 돌리기, Time: 168 ms, Memory: 15968 KB -BaekjoonHub

--- 백준/Gold/17822. 원판 돌리기/원판 돌리기.java
+++ 백준/Gold/17822. 원판 돌리기/원판 돌리기.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    static int n, m, t, map[][], dire[][] = {{1,0},{0,1},{-1,0},{0,-1}}, ret;
+    static int n, m, t, disk[][], ret, dx[] = {1,0,-1,0}, dy[]={0,-1,0,1};
     static boolean visited[][];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,85 +10,72 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         t = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
+        disk = new int[n][m];
         for(int i=0; i<n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j=0; j<m; j++) map[i][j] = Integer.parseInt(st.nextToken());
+            for (int j=0; j<m; j++) disk[i][j] = Integer.parseInt(st.nextToken());
         }
-
-        for (int i=0; i<t; i++) {
+        for(int i=0; i<t; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int d = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
-            rotate(x, d, k);
-            if(!check()) setAvg();
+            int z = Integer.parseInt(st.nextToken());
+            rotate(x,d,z);
+            if(!search()) setAvg();
         }
-
-        for(int i=0; i<n; i++) for (int j=0; j<m; j++) ret += map[i][j];
-
+        for (int i=0; i<n; i++) for (int j=0; j<m; j++) ret += disk[i][j];
         bw.write(String.valueOf(ret));
         bw.close();
     }
-
-    static void rotate(int x, int d, int k) {
+    static void rotate(int x, int d, int z) {
+        z = d==0?z:m-z;
         int temp[] = new int[m];
-        if(d==1) k = m - k;
-        for (int i=x; i<=n; i+=x){
+        for (int i=x; i<=n; i+=x) {
             for (int j=0; j<m; j++) {
-                temp[(j+k)%m] = map[i-1][j];
+                temp[(j+z)%m] = disk[i-1][j];
             }
-            map[i-1] = Arrays.copyOf(temp,m);
+            disk[i-1] = Arrays.copyOf(temp, m);
         }
     }
-
-    static boolean check(){
+    static boolean search() {
         boolean flag = false;
         visited = new boolean[n][m];
-        for(int i=0; i<n; i++) {
+        for (int i=0; i<n; i++) {
             for (int j=0; j<m; j++) {
-                if(map[i][j]==0) continue;
-                if(visited[i][j]) continue;
-                dfs(i, j, map[i][j]);
+                if(visited[i][j]||disk[i][j]==0) continue;
+                dfs(i, j, disk[i][j]);
                 if(visited[i][j]) {
-                    flag = true;
-                    map[i][j] = 0;
+                    flag=true;
+                    disk[i][j] = 0;
                 }
             }
         }
         return flag;
     }
-
-    static void dfs(int y, int x, int target) {
-        for(int i=0; i<4; i++) {
-            int dx = (x + dire[i][0] + m) % m;
-            int dy = y + dire[i][1];
-            if(dy<0||dy>=n||visited[dy][dx]) continue;
-            if(target==map[dy][dx]) {
-                visited[y][x] = visited[dy][dx] = true;
-                dfs(dy, dx, target);
-                map[dy][dx] = 0;
+    static void dfs(int y, int x, int aim) {
+        for (int i=0; i<4; i++) {
+            int nx = (x+m+dx[i]) % m;
+            int ny = dy[i] + y;
+            if(ny<0||ny>=n||visited[ny][nx]) continue;
+            if(aim == disk[ny][nx]) {
+                visited[y][x] = visited[ny][nx] = true;
+                dfs(ny, nx, aim);
+                disk[ny][nx] = 0;
             }
         }
     }
-
     static void setAvg() {
-        double cnt, total;
-        cnt = total = 0;
-        for(int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                if(map[i][j]==0) continue;
-                cnt++;
-                total += map[i][j];
-            }
+        double sum, cnt;
+        sum = cnt =0;
+        for (int i=0; i<n; i++) for (int j=0; j<m; j++) if(disk[i][j]!=0) {
+            cnt++;
+            sum += disk[i][j];
         }
-        double avg = total / cnt;
-        for(int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                if(map[i][j]==0) continue;
-                if(avg<(double) map[i][j]) map[i][j]--;
-                else if(avg>(double) map[i][j]) map[i][j]++;
-            }
+        sum = sum / cnt;
+        for (int i=0; i<n; i++) for (int j=0; j<m; j++) {
+            if(disk[i][j]==0) continue;
+            if((double)disk[i][j]>sum) disk[i][j]--;
+            else if ((double)disk[i][j]<sum) disk[i][j]++;
         }
     }
 }
