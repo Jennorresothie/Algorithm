e2f67c7fa21123906d09d3a74a66d572673eec6a
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-05-15 03:10:05 +0900

    [Silver I] Title: 보석 상자, Time: 368 ms, Memory: 35176 KB -BaekjoonHub

---
+++ 백준/Silver/2792. 보석 상자/보석 상자.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int child, jewelry, j[], l, r, mid, ret=Integer.MAX_VALUE;
+        mid = l = r = 1;
+        child = Integer.parseInt(st.nextToken());
+        jewelry = Integer.parseInt(st.nextToken());
+        j = new int[jewelry];
+        for(int i=0; i<jewelry; i++) {
+            j[i] = Integer.parseInt(br.readLine());
+            if(r<j[i]) r = j[i];
+        }
+        while(l<=r) {
+            mid = (l+r) / 2;
+            int cnt = 0;
+            for (int i : j) {
+                cnt += i/mid;
+                if(i%mid>0) cnt++;
+            }
+            if(cnt>child) l = mid+1;
+            else {
+                r = mid-1;
+                if(ret>mid) ret = mid;
+            }
+        }
+        bw.write(String.valueOf(ret));
+        bw.close();
+    }
+}
