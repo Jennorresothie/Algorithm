b20d69650f49a0b4fc48d12644998e70571a212c
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-06-11 16:53:23 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 696 ms, Memory: 55544 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.java
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.java
@@ -1,47 +1,47 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static long n, atk, l=1, r;
-    static List<int[]>  list = new ArrayList<>();
+
+    static long l=1, r=1;
+    static int room[][], n, ack;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        atk = Integer.parseInt(st.nextToken());
+        ack = Integer.parseInt(st.nextToken());
+        room = new int[n][3];
         for(int i=0; i<n; i++) {
             st = new StringTokenizer(br.readLine());
-            int t = Integer.parseInt(st.nextToken());
-            int a = Integer.parseInt(st.nextToken());
-            int h = Integer.parseInt(st.nextToken());
-            list.add(new int[]{t,a,h});
-            if(t==1) r += ((long)a*h);
+            for(int j=0; j<3; j++) room[i][j] = Integer.parseInt(st.nextToken());
+            if(room[i][0]==1) r += (long)(room[i][2]/ack)*room[i][1];
         }
         while(l<r) {
             long mid = (l+r)/2;
-            if(check(mid))
-                r = mid;
-            else l = mid + 1;
+            if(check(mid)) r = mid;
+            else l = mid+1;
         }
         bw.write(String.valueOf(r));
         bw.close();
     }
+
     static boolean check(long mid) {
-        long Atk = atk;
+        long attack = ack;
         long hp = mid;
 
-        for(int[] room : list) {
-            if(room[0]==1) {
-                long damage = room[2]%Atk==0?(room[2]/Atk)-1:room[2]/Atk;
-                hp -= (damage*room[1]);
+        for(int[] r : room) {
+            if(r[0]==1) {
+                long damage = r[2]%attack==0?(r[2]/attack)-1:r[2]/attack;
+                hp-=damage*r[1];
                 if(hp<=0) return false;
             }
-            else {
-                Atk += room[1];
-                hp += room[2];
+            else{
+                attack += r[1];
+                hp += r[2];
                 if(hp>mid) hp = mid;
             }
         }
         return true;
     }
+
 }
