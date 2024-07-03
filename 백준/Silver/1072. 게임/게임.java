d4a65d381bf0a56e15600e64fc11726dd40a252d
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-07-03 11:32:39 +0900

    [Silver III] Title: 게임, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.java
+++ 백준/Silver/1072. 게임/게임.java
@@ -4,19 +4,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        long r=1000000000, l=1, x, y, ret=-1, avg;
+        long x, y, z, r, l=1, ret=-1;
         StringTokenizer st = new StringTokenizer(br.readLine());
         x = Integer.parseInt(st.nextToken());
         y = Integer.parseInt(st.nextToken());
-        avg = (y*100)/x;
+        z = (y*100)/x;
+        r = 1000000000;
         while(l<=r) {
             long mid = (l+r)/2;
             long temp = (y+mid)*100/(x+mid);
-            if(temp>avg) {
-                ret=mid;
-                r=mid-1;
+            if(temp>z) {
+                ret = mid;
+                r = mid-1;
             }
-            else l = mid+1;
+            else l=mid+1;
         }
         bw.write(String.valueOf(ret));
         bw.close();
