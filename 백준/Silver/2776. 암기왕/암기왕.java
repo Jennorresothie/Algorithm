fe86ea689e6659eb72fd1b3b33df8fac5cf7dbc0
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-07-04 14:16:16 +0900

    [Silver IV] Title: 암기왕, Time: 1608 ms, Memory: 279420 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,39 +1,24 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int [] arr1, arr2;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        arr1 = new int[1000001];
-        arr2 = new int[1000001];
         int t = Integer.parseInt(br.readLine());
         for (int test=0; test<t; test++) {
             int n = Integer.parseInt(br.readLine());
+            Set<Integer> set = new HashSet<>();
             st = new StringTokenizer(br.readLine());
             for (int i=0; i<n; i++)
-                arr1[i] = Integer.parseInt(st.nextToken());
+                set.add(Integer.parseInt(st.nextToken()));
             int m = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
-            for (int i=0; i<m; i++)
-                arr2[i] = Integer.parseInt(st.nextToken());
-            Arrays.sort(arr1, 0, n);
             for (int i=0; i<m; i++) {
-                if(search(n, arr2[i])) bw.write(String.valueOf("1\n"));
-                else bw.write(String.valueOf("0\n"));
+                if(set.contains(Integer.parseInt(st.nextToken()))) bw.write("1\n");
+                else bw.write("0\n");
             }
         }
         bw.close();
-    }
-    static boolean search(int n, int num) {
-        int l=0, r=n-1;
-        while(l<=r) {
-            int mid = (l+r)/2;
-            if(num==arr1[mid]) return true;
-            if(num>arr1[mid]) l = mid+1;
-            else r=mid-1;
-        }
-        return false;
     }
 }
