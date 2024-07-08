9b83d78c0a9bfbd512f10a27bf89aa48c4d269e4
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-07-09 02:27:23 +0900

    [Silver II] Title: 파닭파닭, Time: 580 ms, Memory: 75876 KB -BaekjoonHub

--- 백준/Silver/14627. 파닭파닭/파닭파닭.java
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.java
@@ -1,33 +1,31 @@
 import java.io.*;
 import java.util.*;
 public class Main {
+    static long c, total, l=1, r, ret;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int s = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int[] arr = new int[s];
-        long total=0;
+        c = Integer.parseInt(st.nextToken());
+        int arr[] = new int[s];
         for (int i=0; i<s; i++) {
             arr[i] = Integer.parseInt(br.readLine());
-            total+=arr[i];
+            total += arr[i];
         }
-
-        int l=1, r=1000000000, ret=0;
-        while(l<=r){
-            int mid = (l+r)/2;
-            int sum=0;
-            for (int i : arr)
-                sum+=i/mid;
-            if(sum<c) r = mid-1;
+        r=1000000000;
+        while(l<=r) {
+            long mid = (l+r)/2;
+            int cnt = 0;
+            for (int n : arr) cnt += n/mid;
+            if(cnt<c) r=mid-1;
             else {
-                l = mid+1;
+                l=mid+1;
                 ret = mid;
             }
         }
-        total-=(long)ret*c;
-        bw.write(String.valueOf((total)));
+        total -= (ret*c);
+        bw.write(String.valueOf(total));
         bw.close();
     }
 }
