591fd923fc2480a2d4c59c84f942e36c4aa6c008
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-07-30 03:47:52 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 984 ms, Memory: 239980 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    // LIS
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Integer> list = new ArrayList<>(); // 현재 몇번째 큰 수 인가 판별용
+        int arr[] = new int[n]; // 크기 저장용
+        int nums[] = new int[n];
+        for (int i=0; i<n; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        list.add(0);
+        int ind = 0;
+        for (int e : nums) {
+            if(list.size()==1||list.get(list.size()-1)<e) { // 큰 값이면 그냥 넣기
+                list.add(e);
+                arr[ind++] = list.size() - 1;
+            }
+            else {
+                int l = 1;
+                int r = list.size();
+
+                while(l<r) {
+                    int mid = (l+r)>>1;
+                    if(list.get(mid)>=e) r = mid;
+                    else l = mid+1;
+                }
+                list.set(r, e);
+                arr[ind++] = r;
+            }
+        }
+
+        int len  = list.size() - 1;
+        Deque<Integer> dq = new ArrayDeque<>();
+        sb.append(len + "\n");
+        for(int i=n-1; i>-1; i--) {
+            if(len == arr[i]) {
+                len--;
+                dq.addFirst(nums[i]);
+            }
+        }
+
+        while (!dq.isEmpty()) {
+            sb.append(dq.pop() + " ");
+        }
+        bw.write(sb.toString());
+        bw.close();
+    }
+}
