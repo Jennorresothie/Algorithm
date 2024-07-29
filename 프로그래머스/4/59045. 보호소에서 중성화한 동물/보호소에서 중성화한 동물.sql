1c7d5f49aea415a67a244c0912cbb141d2857a2a
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-07-29 23:57:57 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,14 @@
+-- 코드를 입력하세요
+SELECT
+    i.animal_id,
+    i.animal_type,
+    i.name
+from
+animal_ins as i
+join
+animal_outs as o
+on i.animal_id=o.animal_id
+where
+    i.sex_upon_intake like 'Intact%'
+and
+    (o.sex_upon_outcome like 'Neutered%' or o.sex_upon_outcome like 'Spayed%')
