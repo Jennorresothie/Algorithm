716cb8090f07e89c9e942ff3682bf12e6f4e66f5
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-08-17 13:49:23 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 768 ms, Memory: 186728 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -1,54 +1,48 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    // LIS
-    static int n, arr[], arr2[], nums[], ind, len;
+    static int arr[], indexes[], index;
+    static ArrayList<Integer> list = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        indexes = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        List<Integer> list = new ArrayList<>(); // 현재 몇번째 큰 수 인가 판별용
-        arr = new int[n]; // 크기 저장용
-        nums = new int[n];
-        for (int i=0; i<n; i++){
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
-        list.add(0);
-        for (int e : nums) {
-            if(list.size()==1||list.get(list.size()-1)<e) { // 큰 값이면 그냥 넣기
-                list.add(e);
-                arr[ind++] = list.size() - 1;
-            }
-            else {
-                int l = 1;
-                int r = list.size();
+        for(int i=0; i<n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        for(int i=0; i<n; i++) {
+            int a = arr[i];
+            if(list.isEmpty()||list.get(list.size()-1)<a) {
+                list.add(a);
+                indexes[i] = index++;
+            } else {
+                int l = 0;
+                int r = list.size()-1;
 
                 while(l<r) {
-                    int mid = (l+r)>>1;
-                    if(list.get(mid)>=e) r = mid;
-                    else l = mid+1;
+                    int mid = (l+r) >> 1;
+                    if(list.get(mid)>=a) r=mid;
+                    else l = mid + 1;
                 }
-                list.set(r, e);
-                arr[ind++] = r;
+                list.set(r, a);
+                indexes[i] = r;
             }
         }
 
-        len  = list.size() - 1;
-        arr2 = new int[len];
-        sb.append(len).append("\n");
-        for(int i=n-1; i>-1; i--) {
-            if(len == arr[i]) {
-                len--;
-                arr2[len] = nums[i];
-            }
+        int len = list.size();
+        bw.write(String.valueOf(len)+'\n');
+        int answer[] = new int[len--];
+        for(int i=n-1; i>=0; i--) {
+            if(len==indexes[i])
+                answer[len--] = arr[i];
         }
 
-        for(int i : arr2)
-            sb.append(i).append(" ");
+        for(int a: answer)
+            bw.write(String.valueOf(a)+' ');
 
-        bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
