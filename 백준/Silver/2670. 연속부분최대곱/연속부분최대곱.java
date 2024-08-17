8461886ef93f5ce3d344922b20525b5c921c287f
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-08-17 13:07:47 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 144 ms, Memory: 18396 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.java
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.java
@@ -1,22 +1,19 @@
 import java.io.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int n = Integer.parseInt(br.readLine());
-        double arr[] = new double[n];
-        double max = Double.MIN_VALUE;
 
-        for(int i=0; i<n; i++)
-            arr[i] = Double.parseDouble(br.readLine());
-
-        for (int i=0; i<n; i++) {
-            double temp = 1.0;
-            for (int j=i; j<n; j++) {
-                temp *= arr[j];
-                max = Math.max(temp, max);
-            }
+        double temp = 1.0;
+        double max = -1.0;
+        for(int i=0; i<n; i++) {
+            double ele = Double.parseDouble(br.readLine());
+            temp *= ele;
+            if(temp<ele)
+                temp = ele;
+            max = Math.max(temp, max);
         }
 
         bw.write(String.format("%.3f", max));
