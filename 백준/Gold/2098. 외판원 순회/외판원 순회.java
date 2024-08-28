7e0f84f201ce2c1133fc7f9b33536668c6e25690
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-08-29 00:39:54 +0900

    [Gold I] Title: 외판원 순회, Time: 168 ms, Memory: 20052 KB -BaekjoonHub

---
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int n, map[][], dp[][];
+    static final int INF = 987654321;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        dp = new int[n][1<<n];
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++)
+                map[i][j] = Integer.parseInt(st.nextToken());
+        }
+        int ret = tsp(0,1);
+        bw.write(String.valueOf(ret));
+        bw.flush();
+        bw.close();
+    }
+    static int tsp(int cur, int visited) {
+        if(visited == (1<<n)-1){
+            // cur -> 0(출발도시)로 가는 경로가 존재해야 돌아갈 수 있음
+            if(map[cur][0]>0) return map[cur][0];
+            return INF;
+        }
+            
+
+        if(dp[cur][visited] > 0) return dp[cur][visited];
+
+        dp[cur][visited] = INF;
+        for(int i=0; i<n; i++) {
+            // 방문 체크
+            if((visited & (1<<i)) > 0) continue;
+            // 길 유무 체크
+            if(map[cur][i]==0) continue;
+
+            dp[cur][visited] = Math.min(dp[cur][visited], tsp(i, visited | (1<<i))+ map[cur][i]) ;
+        }
+        return dp[cur][visited];
+    }
+}
