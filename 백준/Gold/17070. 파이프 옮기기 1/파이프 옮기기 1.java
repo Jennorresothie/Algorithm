b6d030480666fd6b63d4bc55b88cbc1c1337d389
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-09-02 19:06:14 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 344 ms, Memory: 18448 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,52 +1,56 @@
 import java.io.*;
 import java.util.*;
 public class Main  {
-    static int n, dp[][][], dire[][]={{0,1},{1,1},{1,0}}, ret;
-    static char map[][];
+    static int n, dp[][][], dire[][] = {{0,1},{1,1},{1,0}}, ret;
+    static boolean map[][];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         n = Integer.parseInt(br.readLine());
-        map = new char[n][n];
+        map = new boolean[n][n];
+        dp= new int[3][n][n];
+
         for(int i=0; i<n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j=0; j<n; j++)
-                map[i][j] = st.nextToken().charAt(0);
+            for(int j=0; j<n; j++)
+                if(st.nextToken().charAt(0)=='0') map[i][j] = true;
         }
-
-        dp = new int[n][n][3];
-        dfs(0, 1, 0);
-        for (int i=0; i<3; i++) ret += dp[n-1][n-1][i];
-        bw.write(ret+"");
+        dpFun(0, 1, 0);
+        ret = dp[0][n-1][n-1] + dp[1][n-1][n-1] + dp[2][n-1][n-1];
+        bw.write( ret+"");
         bw.flush();
         bw.close();
     }
-    static void dfs(int y, int x, int type) {
-        if(y==n-1&&x==n-1) return;
+
+    static void dpFun(int y, int x, int type) {
+        if(y==n-1 && x==n-1)
+            return;
 
         for(int i=-1; i<2; i++) {
             int ind = type + i;
-            if(ind>=0&&ind<=2) {
+
+            if(ind>-1&&ind<3) {
                 int dx = x + dire[ind][1];
                 int dy = y + dire[ind][0];
 
-                if(dx>=n||dy>=n||map[dy][dx]=='1')
-                    continue;
-                if(ind==1&&!check(y, x))
+                if(dx>=n||dy>=n||map[dy][dx]==false)
                     continue;
 
-                dp[dy][dx][ind]++;
-                dfs(dy, dx, ind);
+                if(ind==1&&!check(y,x))
+                    continue;
+
+                dp[ind][dy][dx]++;
+                dpFun(dy, dx, ind);
             }
         }
     }
 
     static boolean check(int y, int x) {
-        for (int i=0; i<3; i++) {
-            int dx = x + dire[i][1];
+        for(int i=0; i<3; i++) {
             int dy = y + dire[i][0];
+            int dx = x + dire[i][1];
 
-            if(dx>=n||dy>=n||map[dy][dx]=='1')
+            if(dx<0||dx>=n||dy<0||dy>=n||map[dy][dx]==false)
                 return false;
         }
         return true;
