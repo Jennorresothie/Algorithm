31b6eeda91c44c5a2ab49f83639dc1b98a914b83
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-09-03 01:50:38 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 108 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -15,42 +15,41 @@ public class Main  {
             for(int j=0; j<n; j++)
                 if(st.nextToken().charAt(0)=='0') map[i][j] = true;
         }
-        dpFun(0, 1, 0);
+        dp[0][0][1] = 1;
+        for (int i=0; i<n; i++) {
+            for (int j=1; j<n; j++) {
+                for (int k=0; k<3; k++) {
+                    for (int l=-1; l<2; l++) {
+                        int ind = l + k;
+
+                        if(ind>=0&&ind<=2) {
+                            int dx = j + dire[ind][1];
+                            int dy = i + dire[ind][0];
+
+                            if(dx>=n||dy>=n||map[dy][dx]==false)
+                                continue;
+
+                            if(ind==1&&!check(i, j))
+                                continue;
+
+                            dp[ind][dy][dx] += dp[k][i][j];
+                        }
+                    }
+                }
+            }
+        }
+
         ret = dp[0][n-1][n-1] + dp[1][n-1][n-1] + dp[2][n-1][n-1];
         bw.write( ret+"");
         bw.flush();
         bw.close();
     }
-
-    static void dpFun(int y, int x, int type) {
-        if(y==n-1 && x==n-1)
-            return;
-
-        for(int i=-1; i<2; i++) {
-            int ind = type + i;
-
-            if(ind>-1&&ind<3) {
-                int dx = x + dire[ind][1];
-                int dy = y + dire[ind][0];
-
-                if(dx>=n||dy>=n||map[dy][dx]==false)
-                    continue;
-
-                if(ind==1&&!check(y,x))
-                    continue;
-
-                dp[ind][dy][dx]++;
-                dpFun(dy, dx, ind);
-            }
-        }
-    }
-
     static boolean check(int y, int x) {
         for(int i=0; i<3; i++) {
             int dy = y + dire[i][0];
             int dx = x + dire[i][1];
 
-            if(dx<0||dx>=n||dy<0||dy>=n||map[dy][dx]==false)
+            if(dx>=n||dy>=n||map[dy][dx]==false)
                 return false;
         }
         return true;
