0de9420118b79d684b8799fcf8113c665bfbe1c7
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-09-09 02:22:40 +0900

    [Gold V] Title: 동전 2, Time: 112 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,42 +1,30 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static final int INF = 100_001;
-    static int dp[], coins[], n;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
+
+        int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        dp = new int[k+1];
-        coins = new int[n];
+        int coins[] = new int[n];
+        int dp[] = new int[k+1];
+        for (int i=0; i<n; i++) coins[i] = Integer.parseInt(br.readLine());
+
+        Arrays.fill(dp, 100001);
+        dp[0] = 0;
 
         for(int i=0; i<n; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
-            if (coins[i]<=k)
-                dp[coins[i]] = 1;
+            int temp = coins[i];
+            for (int j=temp; j<=k; j++) {
+                dp[j] = Math.min(dp[j], dp[j-temp]+1);
+            }
         }
 
-        int ret = search(k);
-        ret = ret>=INF?-1: ret;
-
-        bw.write(ret+"");
+        bw.write((dp[k]==100001?-1:dp[k])+"");
         bw.flush();
         bw.close();
-    }
-
-
-    static int search(int t) {
-        if(dp[t]>0) return dp[t];
-
-        dp[t] = INF;
-        for(int i=0; i<n; i++) {
-            if(t<coins[i]) continue;
-
-            dp[t] = Math.min(dp[t], search(t-coins[i]) + 1 );
-        }
-        return dp[t];
     }
 }
