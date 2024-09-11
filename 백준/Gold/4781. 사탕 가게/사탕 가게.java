27ac0c92b8a0b561790fb079452b96f76da13e16
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-09-11 09:59:49 +0900

    [Gold IV] Title: 사탕 가게, Time: 540 ms, Memory: 28844 KB -BaekjoonHub

--- 백준/Gold/4781. 사탕 가게/사탕 가게.java
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.java
@@ -1,35 +1,35 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int n, dp[] = new int[10001];
-
+    static int dp[];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
+
         while(true) {
-            st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            double temp = Double.parseDouble(st.nextToken());
-            int k = (int) (temp * 100 + 0.5);
-            if(n==0)
-                break;
+            st = new StringTokenizer(br.readLine()," .");
+            int n = Integer.parseInt(st.nextToken());
+            int b1 = Integer.parseInt(st.nextToken());
+            int b2 = Integer.parseInt(st.nextToken());
+            int budget = b1*100 + b2;
+
+            if(n==0) break;
+
+            dp = new int[budget+1];
 
             for(int i=0; i<n; i++) {
-                st = new StringTokenizer(br.readLine());
-                
-                int w = Integer.parseInt(st.nextToken());
-                double price = Double.parseDouble(st.nextToken());
-                int p = (int)(price*100+0.5);
+                st = new StringTokenizer(br.readLine()," .");
+                int cal = Integer.parseInt(st.nextToken());
+                int p1 = Integer.parseInt(st.nextToken());
+                int p2 = Integer.parseInt(st.nextToken());
+                int price = p1*100+p2;
 
-                for (int j=p; j<=k; j++) {
-                    dp[j] = Math.max(dp[j], dp[j-p]+w);
+                for(int j=price; j<=budget; j++) {
+                    dp[j] = Math.max(dp[j], dp[j-price] + cal);
                 }
             }
-
-            bw.write(dp[k]+"\n");
-
-            Arrays.fill(dp, 0);
+            bw.write(dp[budget]+"\n");
         }
         bw.flush();
         bw.close();
