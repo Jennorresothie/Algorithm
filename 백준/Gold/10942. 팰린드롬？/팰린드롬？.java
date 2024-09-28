9506a6cce0810e0fda892dd4fee92121e2d73f97
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-09-28 17:02:51 +0900

    [Gold IV] Title: 팰린드롬?, Time: 932 ms, Memory: 257424 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -1,24 +1,28 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
+
 public class Main {
-    static boolean dp[][];
+    static int dp[][];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        dp = new boolean[n][n];
+        dp = new int[n][n];
         int arr[] = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i=0; i<n; i++) {
-            dp[i][i] = true;
+            dp[i][i] = 1;
             int num = Integer.parseInt(st.nextToken());
             arr[i] = num;
 
-            for(int j=0; j<i; j++) {
-                if(arr[j]==num) dp[j][i] = true;
-            }
+            if(i>0&&arr[i-1]==num) dp[i-1][i] = 1;
         }
+
+        for (int i=2; i<n; i++)
+            for (int j=0; j<n-i; j++)
+                if(arr[j]==arr[j+i]&&dp[j+1][j+i-1]==1)
+                    dp[j][j+i]=1;
 
         int t = Integer.parseInt(br.readLine());
         for (int i=0; i<t; i++) {
@@ -27,14 +31,7 @@ public class Main {
             int a = Integer.parseInt(st.nextToken())-1;
             int b = Integer.parseInt(st.nextToken())-1;
 
-            boolean flag = true;
-            while (a<=b) {
-                if(!dp[a++][b--]) {
-                    flag = false;
-                    break;
-                }
-            }
-            bw.write((flag?1:0)+"\n");
+            bw.write(dp[a][b]+"\n");
         }
         bw.close();
     }
