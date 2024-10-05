e26c4e2f6e94d9295c8fc7464e1661350e79b0c4
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-10-06 02:03:32 +0900

    [Gold V] Title: 1학년, Time: 120 ms, Memory: 15812 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.java
+++ 백준/Gold/5557. 1학년/1학년.java
@@ -1,31 +1,28 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int n, last, arr[];
+    static int n, arr[];
     static long dp[][];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n-1];
-        dp = new long[n-1][21];
+        n=Integer.parseInt(br.readLine());
+        arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i=0; i<n-1; i++) arr[i] = Integer.parseInt(st.nextToken());
-        last = Integer.parseInt(st.nextToken());
+        for (int i=0; i<n; i++) arr[i]=Integer.parseInt(st.nextToken());
+        dp = new long[n-1][21];
 
-        bw.write(go(1, arr[0])+"");
+        bw.write(go(1,arr[0])+"");
         bw.close();
     }
     static long go(int here, int sum) {
-        if(sum<0 || sum>20) return 0;
+        if(sum<0||sum>20) return 0;
         if(here==n-1) {
-            if(sum==last)
-                return 1;
+            if(arr[here]==sum) return 1;
             return 0;
         }
-
-        if(dp[here][sum]!=0) return dp[here][sum];
+        if(dp[here][sum]>0) return dp[here][sum];
 
         dp[here][sum]+=go(here+1, sum+arr[here]);
         dp[here][sum]+=go(here+1, sum-arr[here]);
