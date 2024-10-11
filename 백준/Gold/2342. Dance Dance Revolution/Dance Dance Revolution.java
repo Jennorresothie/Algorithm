564e17c794b4c1994440e98a9fc9485e641e0813
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-10-12 02:44:43 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 576 ms, Memory: 59928 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.java
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 public class Main {
     static final int INF = 987654321;
-    static int arr[] = new int[100_001], len, dp[][][], ret=INF;
+    static int arr[] = new int[100_001], len, dp[][][];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
