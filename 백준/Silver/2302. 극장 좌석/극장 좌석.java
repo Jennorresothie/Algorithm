84c3c69c7215cb6313761fbf47d5154a3e5e0434
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-11-01 16:25:10 +0900

    [Silver I] Title: 극장 좌석, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.java
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 public class Main {
     static int n, m, dp[] = new int[41];
+    static boolean change[] = new boolean[41];
     static ArrayList<Integer> list = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,27 +13,26 @@ public class Main {
         int prev = 0;
         for (int i=0; i<m; i++) {
             int temp = Integer.parseInt(br.readLine());
-            list.add(temp - prev - 1);
-            prev = temp;
+            change[temp - 1] = true;
         }
-        list.add(n - prev);
 
-        dp[1]=1;
-        dp[2]=2;
-        int ret = 1;
-        for (Integer i : list) ret *= go(i);
+        Arrays.fill(dp, -1);
 
-        bw.write(ret+"");
+        bw.write(go(0)+"");
         bw.flush();
         bw.close();
         br.close();
     }
 
     static int go(int num) {
-        if(num==0) return 1;
-        if(dp[num]!=0) return dp[num];
+        if(num >=n-1) return 1;
+        if(change[num]) return go(num+1);
+        if(dp[num]!=-1) return dp[num];
 
-        dp[num] = go(num-2)+go(num-1);
+        dp[num] = 0;
+        if(!change[num+1]) dp[num] += (go(num+2) + go(num+1));
+        else dp[num] += go(num+1);
+
         return dp[num];
     }
 }
