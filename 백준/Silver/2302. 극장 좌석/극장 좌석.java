98351dfa1ebbfdbf610b62e3d561f834b57d9e02
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-11-01 15:17:06 +0900

    [Silver I] Title: 극장 좌석, Time: 108 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.java
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.java
@@ -1,37 +1,38 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 public class Main {
-    static int dp[] = new int[41];
+    static int n, m, dp[] = new int[41];
+    static ArrayList<Integer> list = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        ArrayList<Integer> list = new ArrayList<>();
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
 
         int prev = 0;
         for (int i=0; i<m; i++) {
-            int num = Integer.parseInt(br.readLine());
-            list.add(num-prev-1);
-            prev = num;
+            int temp = Integer.parseInt(br.readLine());
+            list.add(temp - prev - 1);
+            prev = temp;
         }
-        list.add(n-prev);
+        list.add(n - prev);
 
+        dp[1]=1;
+        dp[2]=2;
         int ret = 1;
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i : list) ret *= go(i);
+        for (Integer i : list) ret *= go(i);
+
         bw.write(ret+"");
         bw.flush();
         bw.close();
         br.close();
     }
 
-    static int go (int num) {
+    static int go(int num) {
         if(num==0) return 1;
         if(dp[num]!=0) return dp[num];
 
-        dp[num] = go(num - 2) +  go(num - 1);
+        dp[num] = go(num-2)+go(num-1);
         return dp[num];
     }
 }
