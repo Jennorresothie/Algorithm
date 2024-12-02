04efa681fb1b94971752c31c40a9da7ed7b5d46d
Jennorresothie <117874399+Jennorresothie@users.noreply.github.com>
2024-12-02 23:32:29 +0900

    [Platinum IV] Title: 나무 심기, Time: 308 ms, Memory: 31432 KB -BaekjoonHub

--- 백준/Platinum/1280. 나무 심기/나무 심기.java
+++ 백준/Platinum/1280. 나무 심기/나무 심기.java
@@ -1,58 +1,51 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    static final int MAX = 200001;
     static final int MOD = 1000000007;
+    static final int MAX = 200001;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        long answer = 1;
-        long tree_cnt[] = new long[MAX+1];
-        long tree_sum[] = new long[MAX+1];
+        long tree_cnt[], tree_sum[];
 
-        Arrays.fill(tree_cnt, 0);
-        Arrays.fill(tree_sum, 0);
+        tree_cnt = new long[MAX+1];
+        tree_sum = new long[MAX+1];
 
-        int test = Integer.parseInt(br.readLine());
-        for (int t=0; t<test; t++) {
-            int n = Integer.parseInt(br.readLine())+1;
-
-            if (t>0) {
-                long left = n * interval(tree_cnt, 1, n-1) - interval(tree_sum, 1, n-1);
-                long right = interval(tree_sum, n+1, MAX) - n * interval(tree_cnt, n+1, MAX);
-
-                answer *= (left + right) % MOD;
+        int t, n;
+        long answer=1;
+        t = Integer.parseInt(br.readLine());
+        for (int i=0; i<t; i++){
+            n = Integer.parseInt(br.readLine())+1;
+            if (i > 0) {
+                long l = n*interval(tree_cnt, 1, n-1) - interval(tree_sum, 1, n-1);
+                long r = interval(tree_sum, n+1, MAX) - n*interval(tree_cnt, n+1, MAX);
+                answer *= (l+r)%MOD;
                 answer %= MOD;
             }
             update(tree_cnt, n, 1);
             update(tree_sum, n, n);
         }
-
         bw.write(answer+"");
         bw.flush();
         bw.close();
         br.close();
     }
-
-    static long prefix(long[] arr, int n) { // 누적함
+    static long prefix(long arr[], int n) {
         long ret = 0;
-        while ( n>0 ) {
+        while (n>0) {
             ret += arr[n];
             n -= (n & -n);
         }
         return ret;
     }
-
-    static long interval(long[] arr, int start, int end) { // 구간합
+    static long interval(long arr[], int start, int end) {
         return prefix(arr, end) - prefix(arr, start-1);
     }
-
-    static void update(long[] arr, int n, int dif) { // 값 추가
-        while ( n < MAX) {
+    static void update(long arr[], int n, int dif) {
+        while(n<MAX) {
             arr[n] += dif;
-            n += (n&-n);
+            n += (n & -n);
         }
     }
 }
